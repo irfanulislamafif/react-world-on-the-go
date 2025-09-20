3aaa67a920f36441e245b586d9a6c32ac512e22c
irfanulislamafif <irfanulislamafif@gmail.com>
2025-09-20 19:45:47 +0600

    the 3rd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-
-import './App.css'
-import Countries from './Components/Countries'
-
+import { Suspense } from "react";
+import "./App.css";
+import Countries from "./Components/Countries/Countries";
+const countriesPromise = fetch(
+  "https://openapi.programming-hero.com/api/all"
+).then((res) => res.json());
 function App() {
-
-
   return (
     <>
-     <Countries></Countries>
-
-     
-
+    <Suspense fallback={<p>Nadir bhai loading...</p>}>
+        <Countries countriesPromise={countriesPromise}></Countries>
+    </Suspense>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/Country/Country.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Country = ({country}) => {
+    
+    return (
+        <div>
+            <img src={country?.flags?.flags?.png} alt="" />
+            <h3>Name: {country.name.common}</h3>
+            <h4>Population: {country.population.population}</h4>
+        </div>
+    );
+};
+
+export default Country;
